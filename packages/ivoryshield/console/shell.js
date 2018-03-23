7d8708a47d25ae917db4c947da392a223b05e03a
Remi Cattiau <remi@cattiau.com>
2018-03-23 01:49:38 -0700

    Polishing the accounts panel

--- packages/ivoryshield/console/shell.js
+++ packages/ivoryshield/console/shell.js
@@ -6,35 +6,75 @@ const AWSServiceMixIn = require("../services/aws-mixin");
 
 class ConfigurationService extends AWSServiceMixIn(Executor) {
   init(params) {
-    super.init(params);
+    this._awsCache = {};
     this._params = this._params || {};
     console.log('init configuration service');
     this._addRoute('/api/credentials', {"method": ["GET", "PUT"], "executor": this._name, "_method": this._restCredentials});
-    this._addRoute('/api/accounts', {"method": ["GET", "PUT"], "executor": this._name, "_method": this.getOrganization});
+    this._addRoute('/api/accounts', {"method": ["GET", "PUT"], "executor": this._name, "_method": this._restAccounts});
+    this._addRoute('/api/organization/enable', {"method": ["PUT"], "executor": this._name, "_method": this.enableOrganization});
+    this._addRoute('/api/organization/disable', {"method": ["PUT"], "executor": this._name, "_method": this.disableOrganization});
     this._addRoute('/api/accounts/test', {"method": ["PUT"], "executor": this._name, "_method": this.testConnection});
     this._addRoute('/api/me', {"method": ["GET"], "executor": this._name, "_method": this.getMe});
-    this._aws = this._getAWS({region: 'us-east-1'});
-    this.reinitClients();
     this.load();
+    this._config.credentials = this._config.credentials || {};
+    this._aws = this._getAWS({region: 'us-east-1', accessKeyId: this._config.credentials.accessKeyId, secretAccessKey: this._config.credentials.secretAccessKey});
+    this.reinitClients();
   }
 
+  _restAccounts(ctx) {
+    if (ctx._route._http.method === 'PUT') {
+      if (!this._config.accounts) {
+        throw 400;
+      }
+      this._config.accounts = ctx.body;
+      return this.save();
+    } else if (ctx._route._http.method === 'GET') {
+      if (this._config.accounts) {
+        ctx.write(this._config.accounts);
+      } else {
+        return this.getOrganization(ctx);
+      }
+    }
+  }
+
+  enableOrganization(ctx) {
+    delete this._config.accounts;
+    return this.save().then( () => {
+      this.organization = new (this._aws.Organizations)().listAccounts({}).promise();
+      return this.getOrganization(ctx);
+    });
+  }
+
+  disableOrganization(ctx) {
+    this._config.accounts = [];
+    return this.save().then( () => {
+      ctx.write(this._config.accounts);
+    });
+  }
 
   reinitClients() {
     this._sts = new this._aws.STS();
     this.mainAccount = this._sts.getCallerIdentity().promise();
-    this.organization =  new (this._aws.Organizations)().listAccounts({}).promise();
+    if (!this._config.accounts) {
+      this.organization = new (this._aws.Organizations)().listAccounts({}).promise();
+    }
   }
 
   testConnection(ctx) {
-    console.log('Testing all', ctx.body);
     this._aws = this._getAWS({region: 'us-east-1', accessKeyId: ctx.body.accessKeyId, secretAccessKey: ctx.body.secretAccessKey});
     this.reinitClients();
+    let promise;
+    if (this._config.accounts) {
+      promise = Promise.resolve({Accounts: this._config.accounts});
+    } else {
+      promise = this.organization;
+    }
     let accounts;
-    return this.organization.then( (res) => {
+    return promise.then( (res) => {
       accounts = res.Accounts;
       let promises = accounts.map( (acc) => {
-        console.log('should test', acc);
         acc.AssumeRoleSuccessful = false;
+        acc.AssumeRoleError = null;
         return this._assumeRole(acc.Id, ctx.body.role, ctx.body.externalId, 'us-east-1', true).then( () => {
           acc.AssumeRoleSuccessful = true;
         }).catch( (err) => {
@@ -51,6 +91,12 @@ class ConfigurationService extends AWSServiceMixIn(Executor) {
   save() {
     return new Promise( (resolve, reject) => {
       this._config.lastUpdate = new Date();
+      if (this._config.accounts) {
+        this._config.accounts.map((acc) => {
+          delete acc.AssumeRoleSuccessful;
+          delete acc.AssumeRoleError;
+        });
+      }
       fs.writeFile('./ivoryshield.config.json', JSON.stringify(this._config, null, ' '), (err, res) => {
         if (err) {
           reject(err);
@@ -79,8 +125,13 @@ class ConfigurationService extends AWSServiceMixIn(Executor) {
   }
 
   getMe(ctx) {
+    let mainAccount;
     return this.mainAccount.then( (acc) => {
-      ctx.write(acc);
+      mainAccount = acc;
+    }).catch( (err) => {
+      console.log('Cannot get the main account');
+    }).then( () => {
+      ctx.write({mainAccount: mainAccount, useOrganization: this._config.accounts === undefined});
     });
   }
 
